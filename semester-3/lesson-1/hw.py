1a6319f3f730c65b43422b90185b5e8c72245f45
Farangis Djumanbayeva <shdjumanbaeva02@gmail.com>
2023-07-13 17:53:09 +0500

    Half of homework done

---
+++ semester-3/lesson-1/hw.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+def parse_vox():
+    results = []
+    for page in range(1, 4):
+        url = f"https://www.vox.com/technology/archives/{page}"
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        titles = soup.find_all(class_="c-entry-box--compact__title")
+
+        for title in titles:
+            results.append(
+                {
+                "title": title.a.string,
+                "link": title.a["href"],
+
+                }
+            )
+    
+    return results
+
+res = parse_vox()
+pprint(res)
